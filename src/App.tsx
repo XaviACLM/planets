80fd5489ed5cb585a7b6c344c856c80957153f84
xaviACLM <xavi.aclm@gmail.com>
2025-10-08 18:35:58 +0200

    planets

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,53 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Clock from './Clock'
 import ZodiacWheel from './ZodiacWheel'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-	  <ZodiacWheel />
-  )
+	
+	const [menuOpen, setMenuOpen] = useState(false);
+	const [showLabels, setShowLabels] = useState(true);
+	
+	return (
+		<div style={{ position: "relative", height: "100vh" }}>
+			<div
+				style={{
+					position: "absolute",
+					top: "1rem",
+					right: "1rem",
+					background: "black",
+					padding: "0.5rem",
+					color: "white",
+					border: "1px solid white",
+					borderRadius: "0.5rem"
+				}}
+			>
+				<button
+					onClick = {() => {setMenuOpen(!menuOpen)}}
+					style={{
+						//position:"absolute",
+						top: "0rem",
+						right: "0rem",
+						color: "white",
+						background: "black",
+					}}
+				>
+					☰
+				</button>
+				
+				{menuOpen && (
+					<label>
+						<input
+							type="checkbox"
+							checked={showLabels}
+							onChange={() => setShowLabels(!showLabels)}
+						/>
+						Show labels
+					</label>
+				)}
+			</div>
+			<ZodiacWheel />
+			
+		</div>
+	)
 }
 
 export default App
--- src/ZodiacWheel.tsx
+++ src/ZodiacWheel.tsx
@@ -1,29 +1,102 @@
 import React from 'react';
 
-import Aries from "./assets/zodiac-symbols/Aries.png"
-import Taurus from "./assets/zodiac-symbols/Taurus.png"
-import Gemini from "./assets/zodiac-symbols/Gemini.png"
-import Cancer from "./assets/zodiac-symbols/Cancer.png"
-import Leo from "./assets/zodiac-symbols/Leo.png"
-import Virgo from "./assets/zodiac-symbols/Virgo.png"
-import Libra from "./assets/zodiac-symbols/Libra.png"
-import Scorpio from "./assets/zodiac-symbols/Scorpio.png"
-import Sagittarius from "./assets/zodiac-symbols/Sagittarius.png"
-import Capricorn from "./assets/zodiac-symbols/Capricorn.png"
-import Aquarius from "./assets/zodiac-symbols/Aquarius.png"
-import Pisces from "./assets/zodiac-symbols/Pisces.png"
+import { Observer, Body, Equator, GeoVector } from "astronomy-engine";
 
-const zodiacSymbols = [Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius, Pisces];
+import ariesSymbol from "./assets/zodiac-symbols/Aries.png"
+import taurusSymbol from "./assets/zodiac-symbols/Taurus.png"
+import geminiSymbol from "./assets/zodiac-symbols/Gemini.png"
+import cancerSymbol from "./assets/zodiac-symbols/Cancer.png"
+import leoSymbol from "./assets/zodiac-symbols/Leo.png"
+import virgoSymbol from "./assets/zodiac-symbols/Virgo.png"
+import libraSymbol from "./assets/zodiac-symbols/Libra.png"
+import scorpioSymbol from "./assets/zodiac-symbols/Scorpio.png"
+import sagittariusSymbol from "./assets/zodiac-symbols/Sagittarius.png"
+import capricornSymbol from "./assets/zodiac-symbols/Capricorn.png"
+import aquariusSymbol from "./assets/zodiac-symbols/Aquarius.png"
+import piscesSymbol from "./assets/zodiac-symbols/Pisces.png"
+
+import earthSymbol from "./assets/body-symbols/Earth.png"
+import jupiterSymbol from "./assets/body-symbols/Jupiter.png"
+import marsSymbol from "./assets/body-symbols/Mars.png"
+import mercurySymbol from "./assets/body-symbols/Mercury.png"
+import moonSymbol from "./assets/body-symbols/Moon.png"
+import neptuneSymbol from "./assets/body-symbols/Neptune.png"
+import plutoSymbol from "./assets/body-symbols/Pluto.png"
+import saturnSymbol from "./assets/body-symbols/Saturn.png"
+import sunSymbol from "./assets/body-symbols/Sun.png"
+import uranusSymbol from "./assets/body-symbols/Uranus.png"
+import venusSymbol from "./assets/body-symbols/Venus.png"
+
+enum Zodiac{
+  Aries = 'Aries',
+  Taurus = 'Taurus', 
+  Gemini = 'Gemini',
+  Cancer = 'Cancer',
+  Leo = 'Leo',
+  Virgo = 'Virgo',
+  Libra = 'Libra',
+  Scorpio = 'Scorpio',
+  Sagittarius = 'Sagittarius',
+  Capricorn = 'Capricorn',
+  Aquarius = 'Aquarius',
+  Pisces = 'Pisces'
+}
 
 function ZodiacWheel() {
 	
 	const radius = 35; // percent of viewport
-	const symbolRadius = 40; // idem, slightly outside
+	const symbolRadius = 40;
 	const hoveredSymbolRadius = 42;
 	const sectorRadius = 45;
+	const planetRadius = 30;
 	
 	const symbolSize = 4;
 	const strokeWidthPrimary = 0.15;
+	
+	const correct_for_aberration = true;
+	
+	// todo find ascendant (position), lunar ascending, lunar descending
+	// todo aspects (conjunction, opposition, sextile, square, trine)
+	// ...with a nice menu on the side
+	
+	const zodiacSymbols = new Map<Zodiac, string>([
+		[Zodiac.Aries, ariesSymbol],
+		[Zodiac.Taurus, taurusSymbol],
+		[Zodiac.Gemini, geminiSymbol],
+		[Zodiac.Cancer, cancerSymbol],
+		[Zodiac.Leo, leoSymbol],
+		[Zodiac.Virgo, virgoSymbol],
+		[Zodiac.Libra, libraSymbol],
+		[Zodiac.Scorpio, scorpioSymbol],
+		[Zodiac.Sagittarius, sagittariusSymbol],
+		[Zodiac.Capricorn, capricornSymbol],
+		[Zodiac.Aquarius, aquariusSymbol],
+		[Zodiac.Pisces, piscesSymbol]
+	]);
+	
+	const bodySymbols = new Map<Body, string>([
+		[Body.Sun, sunSymbol],
+		[Body.Moon, moonSymbol],
+		[Body.Mercury, mercurySymbol],
+		[Body.Venus, venusSymbol],
+		[Body.Mars, marsSymbol],
+		[Body.Jupiter, jupiterSymbol],
+		[Body.Saturn, saturnSymbol],
+		[Body.Uranus, uranusSymbol],
+		[Body.Neptune, neptuneSymbol],
+		[Body.Pluto, plutoSymbol]
+	]);
+	
+	const zodiac: Body[] = Array.from(zodiacSymbols.keys());
+	const bodies: Body[] = Array.from(bodySymbols.keys());
+	
+	const bodyAngles = new Map<Body, number>();
+	
+	for (let body of bodies) {
+		const v = GeoVector(body, new Date(), correct_for_aberration);
+		bodyAngles.set(body, Math.atan2(v.y, v.x));
+	}
+	
 	
 	const [hovered, setHovered] = React.useState<number | null>(null);
 	
@@ -35,6 +108,16 @@ function ZodiacWheel() {
 				style={{ width: "100%", height: "100%" }}
 			>
 				<circle cx="50%" cy="50%" r={radius} stroke="white" strokeWidth={strokeWidthPrimary} fill="none"/>
+				
+				<image
+					key={-1}
+					href={earthSymbol}
+					x={50-symbolSize/2}
+					y={50-symbolSize/2}
+					width={symbolSize}
+					height={symbolSize}
+					style={{filter:"invert(1)"}}
+				/>
 				
 				{Array.from({ length: 12 }).map((_, i) => {
 					const a = ((2*i+1)/24) * 2 * Math.PI;
@@ -52,7 +135,7 @@ function ZodiacWheel() {
 					);
 				})}
 				
-				{zodiacSymbols.map((symbol, i) => {
+				{zodiac.map((symbol, i) => {
 					const a = (i/12) * 2 * Math.PI;
 					const x = 50 + symbolRadius * Math.cos(a);
 					const y = 50 + symbolRadius * Math.sin(a);
@@ -60,7 +143,7 @@ function ZodiacWheel() {
 					return (
 						<image
 							key={i}
-							href={symbol}
+							href={zodiacSymbols.get(symbol)}
 							x={x-symbolSize/2}
 							y={y-symbolSize/2}
 							width={symbolSize}
@@ -68,6 +151,69 @@ function ZodiacWheel() {
 							transform={`rotate(${r}, ${x}, ${y}) translate(0, ${hovered === i ? -2 : 0})`}
 							style={{ transition: "transform 0.5s ease", filter:"invert(1)"}}
 						/>
+					);
+				})}
+				
+				{zodiac.map((symbol, i) => {
+					const a = ((2*i-1)/24) * 2 * Math.PI +0.01;
+					const x = 50 + sectorRadius * Math.cos(a);
+					const y = 50 + sectorRadius * Math.sin(a);
+					const r = (a * 180) / Math.PI + 180;
+					return (
+						<text
+							key={i}
+							x={x}
+							y={y}
+							width={symbolSize}
+							height={symbolSize}
+							fontSize="1.5"
+							fontWeight="bold"
+							transform={`rotate(${r}, ${x}, ${y})`}
+							style={{filter:"invert(1)", fontVariant: "small-caps"}}
+						>
+							{symbol}
+						</text>
+					);
+				})}
+				
+				{bodies.map((body, i) => {
+					const a = bodyAngles.get(body);
+					const x = 50 + planetRadius * Math.cos(a);
+					const y = 50 + planetRadius * Math.sin(a);
+					const r = (a * 180) / Math.PI + 90;
+					return (
+						<image
+							key={i}
+							href={bodySymbols.get(body)}
+							x={x-symbolSize/2}
+							y={y-symbolSize/2}
+							width={symbolSize}
+							height={symbolSize}
+							transform={`rotate(${r}, ${x}, ${y})`}
+							style={{filter:"invert(1)"}}
+						/>
+					);
+				})}
+				
+				{bodies.map((body, i) => {
+					const a = bodyAngles.get(body);
+					const x = 50 + planetRadius * Math.cos(a);
+					const y = 50 + planetRadius * Math.sin(a);
+					const r = (a * 180) / Math.PI + 180;
+					return (
+						<text
+							key={i}
+							x={x+0.6+symbolSize/2}
+							y={y+0.6}
+							width={symbolSize}
+							height={symbolSize}
+							fontSize="1.5"
+							fontWeight="bold"
+							transform={`rotate(${r}, ${x}, ${y})`}
+							style={{filter:"invert(1)", fontVariant: "small-caps"}}
+						>
+							{body}
+						</text>
 					);
 				})}
 				
