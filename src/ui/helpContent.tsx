b1a3e4ebedecf574d035968aede7148fe3c6fbbc
xaviACLM <xavi.aclm@gmail.com>
2026-02-20 02:17:19 +0100

    updated help menus

--- src/ui/helpContent.tsx
+++ src/ui/helpContent.tsx
@@ -4,6 +4,10 @@ const P: FC<{ children: React.ReactNode }> = ({ children }) => (
 	<p className="mb-2">{children}</p>
 );
 
+const UL: FC<{ children: React.ReactNode }> = ({ children }) => (
+	<ul className="list-disc pl-4">{children}</ul>
+);
+
 // ============================================================================
 // Module Help Content
 // ============================================================================
@@ -39,6 +43,9 @@ export const DominanceChartHelp: FC = () => (
 		<P>
 			<b>Expanded Mode.</b> Segregates the overall analysis into the personal, social, and transpersonal planets. Displays the complete lists of planets in each element and mode.
 		</P>
+		<P>
+			<b>Nodes in analysis.</b> By default analysis involves all planets and luminaries (except those in Ophiuchus if applicable). This can be changed in the settings to be all selected nodes or all selected physical nodes.
+		</P>
 	</>
 );
 
@@ -49,6 +56,9 @@ export const OrientationHelp: FC = () => (
 		</P>
 		<P>
 			Note that these 4 quadrants need not appear square on the main chart, particularly at higher latitudes.
+		</P>
+		<P>
+			<b>Nodes in analysis.</b> By default analysis involves all planets and luminaries. This can be changed in the settings to be all selected nodes or all selected physical nodes.
 		</P>
 	</>
 );
@@ -65,7 +75,7 @@ export const RulershipGraphHelp: FC = () => (
 			<b>Dispositorship Chains.</b> Shows one dispositorship chain for each leaf node (planet ruling no other planet), starting at that planet and ending at the first final dispositor encountered. Chains may share segments; shared segments are repeated.
 		</P>
 		<P>
-			<b>Rulership Scheme.</b> Dispositorship structure depends on the selected dignity mode. Defaults to modern (post-19th-century Western rulerships), but classical (Ptolemaic, from the Tetrabiblos) is also available.
+			<b>Rulership Scheme.</b> Dispositorship structure depends on the selected dignity mode. Defaults to modern (post-19th-century western rulerships), but classical (Ptolemaic, from the Tetrabiblos) is also available.
 		</P>
 	</>
 );
@@ -76,29 +86,29 @@ export const NodeSelectorHelp: FC = () => (
 			This panel allows one to select which nodes are taken into account for analysis.
 		</P>
 		<P>
-			<b>Effects.</b> The nodes selected here will be the nodes that appear on the main chart, the nodes for which aspects are calculated, and the nodes that can be selected in the planet info panel. The selection will not affect the nodes taken into consideration for element/mode analysis, orientation analysis, or rulership structure - these will always be the standard planets and luminaries.
+			<b>Effects.</b> The nodes selected here will be the nodes that appear on the main chart, the nodes for which aspects are calculated, and the nodes that can be selected in the planet info panel. By default, this selection will not affect the nodes taken into consideration for element/mode analysis, orientation analysis, or rulership structure, but this can be changed in the settings.
 		</P>
 		<P>
 			<b>Nodes.</b> The panel allows selection/unselection of:
 		</P>
-		<ul>
-			<li>- All standard planets and luminaries.</li>
-			<li>- All primary angles & other calculated points.</li>
-			<li>- Arabic parts - currently only the Part of Fortune.</li>
-			<li>- The North and South Lunar Nodes, as well as the Lilith and Selene (the lunar apogee and perigee). Note that these are the mean apogee/perigee by default - this can be changed in the settings.</li>
-			<li>- Nearly every astrologically relevant TNO, separated into Major Asteroids & Dwarfs / Minor Bodies.</li>
-			<li>- Hamburg School Objects, i.e. the trans-neptunian hypotheticals. The settings allow one to select between the Witte/Siegrrün and Neely (default) parameter sets, as well as whether these objects are considered physical (for aspect detection). </li>
-		</ul>
+		<UL>
+			<li>All standard planets and luminaries.</li>
+			<li>All primary angles & other calculated points.</li>
+			<li>Arabic parts - the more frequent ones in modern western astrology, all traced back to either Dorotheus of Sidon or Paulus Alexandrinus (in particular - DS and PA provide different definitions for a part of "Eros", we go with the Dorothean definition).</li>
+			<li>The North and South Lunar Nodes, as well as the Lilith and Selene (the lunar apogee and perigee). Note that these are the mean (via Meeus) positions by default - this can be changed in the settings.</li>
+			<li>Nearly every astrologically relevant TNO, separated into Major Asteroids & Dwarfs / Minor Bodies.</li>
+			<li>Hamburg School Objects, i.e. the trans-neptunian hypotheticals. The settings allow one to select between the Witte/Siegrrün and Neely (default) parameter sets, as well as whether these objects are considered physical (for aspect detection). </li>
+		</UL>
 	</>
 );
 
 export const GeneralSettingsHelp: FC = () => (
 	<>
 		<P>
-			This is the settings panel, exposing options relating to which astrological conventions are followed, and for how the App is rendered visually.
+			This is the settings panel, exposing options relating to which astrological conventions are followed, and to how the App is rendered visually.
 		</P>
 		<P>
-			<b>Houses.</b> Allows for the selection of a specific house system. Also allows for toggling of the "house presweep" rule, where house cusps are understood to be 5 degrees into a house.
+			<b>Houses.</b> Allows for the selection of a specific house system. Also allows for toggling of the "house presweep" rule, where houses are defined to begin 5 degrees before their cusp.
 		</P>
 		<P>
 			<b>Labeling.</b> Allows for selection of whether each category of items with icons (nodes, zodiac symbols, etc) will prefer to be displayed as a label or as an icon. Note that this is not universal, e.g. the main chart will always use icons and nodes-within-text will always prefer rendering as text.
@@ -113,13 +123,13 @@ export const GeneralSettingsHelp: FC = () => (
 			<b>Other Aspect Options.</b> Allows for the specification of how many physical nodes an aspect needs to be valid - the default value is "All but one", meaning at most one node can be non-physical in each aspect. Allows for the toggling of whether Hamburg School Objects should be treated as physical. Also allows for selection of aspect display mode - in the standard mode, "Maximal with submenus", each configuration will show beneath it its subaspects (e.g. a grand trine would be indicated to "contain" three trines). With "Only maximal", these subaspects are discarded (a trine does not appear by itself if it belongs to a grand trine. With "All", all aspects are shown with no hierarchy.
 		</P>
 		<P>
-			<b>Lunar Nodes.</b> Allows for the selection of lunar node calculation mode. "True" uses astronomical calculation to figure out the true instantaneous position of all lunar nodes. "Mean" uses Meeus' polynomials to obtain the mean, non-osculating positions of the lunar nodes.
+			<b>Lunar Nodes.</b> Allows for the selection of lunar node calculation mode. "True" uses astronomical calculation to figure out the true instantaneous position of all lunar nodes. "Mean" uses Meeus' polynomials to obtain the mean, non-oscillating positions of the lunar nodes.
 		</P>
 		<P>
 			<b>Astrology Mode.</b> Defaults to tropical astrology, but also allows the selection of sidereal astrology (with all major ayanamsa) and 13-sign constellation-based astrology. Note that "Constellations - IAU / Berg" is the standard system promoted by Walter Berg where the ecliptic is sliced up according to the IAU-defined constellation boundaries. "Constellations - Closest" is a mode specific to this app wherein the sign of each position along the ecliptic is decided by which constellation (as a graph on the stars, from Hipparchos' tables) is closest.
 		</P>
 		<P>
-			<b>Dignities.</b> Allows for the selection of a standard set of dignities. "Modern" is the standard dignity definitions for post-19th-century Western astrology, all other tables of values are from the Tetrabiblos. Note that the selected dignity mode will decide the rulership tables, and thus affect the dispositorship graph.
+			<b>Dignities.</b> Allows for the selection of a standard set of dignities. "Modern" is the standard dignity definitions for post-19th-century western astrology, all other tables of values are from the Tetrabiblos. Note that the selected dignity mode will decide the rulership tables, and thus affect the dispositorship graph.
 		</P>
 		<P>
 			<b>Stationary nodes.</b> Allows for the setting of a percentual threshold - nodes moving below this percentage of their average speed will be considered stationary.
@@ -139,13 +149,19 @@ export const AspectMenuHelp: FC = () => (
 			<b>Node Selection.</b> Nodes may be selected by clicking on them in the main chart - this will cause this panel to show only aspects incident on the selected node. Nodes may be unselected by clicking anywhere on the main chart area, or on the "Show all aspects" button that will appear on top of this panel.
 		</P>
 		<P>
-			<b>Aspect Data.</b> For each aspect, this panel shows the aspect kind, the nodes involved, and the orb.
+			<b>Aspects displayed.</b> Information will be displayed about all aspects between selected nodes that match the filters specified in the settings. Configurations will be displayed first, ordered by amount of nodes. Beyond this, aspects are always ordered by orb (increasing).
 		</P>
 		<P>
-			<b>Hierarchy.</b> By default, aspects that are "included" in another aspect (e.g. a trine in a grand trine) are shown indented below said aspect. This may be changed in the settings.
+			<b>Hierarchy.</b> There is a notion of an aspect being contained in another (e.g. a grand trine contains three trines). This panel uses this idea, in particular displaying subaspects below their parent configuration. This can be changed in the settings (showing all aspects in a flat list, or not showing any subaspects).
 		</P>
 		<P>
-			<b>Interactability.</b> Hovering over any aspect will highlight this aspect in the main chart. In particular hovering over a parallel/contraparallel will also show the line connecting the two equinoxes, analogous to the celestial equator. Aspects may be discarded by clicking on the "X" button, though these may come back if the aspects need to be recomputed (e.g., node selection changes).
+			<b>Configurations.</b> Configurations are displayed in a mini-diagram, with all the subaspects below. These can be hovered to highlight them both in the mini-diagram and main chart.
+		</P>
+		<P>
+			<b>Binary aspects.</b> Binary aspects are displayed as a list before the configurations, listing their aspect kind, nodes, and orb.
+		</P>
+		<P>
+			<b>Interactability.</b> Hovering over any aspect will highlight this aspect in the main chart. In particular hovering over a parallel/contraparallel will also show the line connecting the two equinoxes, analogous to the celestial equator. Aspects may be discarded by clicking on the "X" button, but they will come back upon recomputation (changes in date/location or settings that change node positions).
 		</P>
 	</>
 );
